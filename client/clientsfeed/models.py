72875a04f2b8091bebfbb1e35bbc5c27a2af006e
Anudeep Samaiya <anudeepsamaiya@gmail.com>
2019-06-21 17:10:18 +0530

    create reverse generic relation with another model

---
+++ client/clientsfeed/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.contenttypes.fields import GenericRelation
+
+from rest_auth.models import UserFollow
+
+class Source(models.Model):
+    name = models.CharField(max_length=200, null=True, blank=True)
+    follower = GenericRelation(UserFollow, related_query_name="sources")
